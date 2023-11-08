af6871bf2d977dab288c453f4b54ee87d7e00499
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2023-11-07 23:09:58 -0500

    fixed base code

--- base-code/main.rb
+++ base-code/main.rb
@@ -5,9 +5,11 @@ data = File.read(file_name).split("\n").map { |i| i.strip }
 
 def part1(input)
     return input
+end
 
 def part2(input)
     return input
+end
 
 puts part1(data)
 # puts part2(data)
