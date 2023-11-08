b84792a6e288ce65d53af2f66a6db890dae728b9
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2023-11-07 23:09:23 -0500

    day 1 solutions

--- base-code/main.rb
+++ base-code/main.rb
@@ -1,5 +1,13 @@
 file_name = 'sampleIn.txt'
-file_name = 'input.txt'
+# file_name = 'input.txt'
 
 data = File.read(file_name).split("\n").map { |i| i.strip }
 
+def part1(input)
+    return input
+
+def part2(input)
+    return input
+
+puts part1(data)
+# puts part2(data)
